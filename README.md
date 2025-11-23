8890b3b84b7368862b91e0d5a8bd3d21ff8a02d5
lex0tan <forvypr1@gmail.com>
2025-11-24 00:17:13 +0100

    messi placeholder nella pagina principale, creato ordini++ con tabella per ordini, aggiunto js per modificare lo stato dell'ordine

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+zioperone 
---
+++ source/js/main.js
@@ -0,0 +1,11 @@
+// document.querySelector(".newOrder").addEventListener("click", function(){
+//     console.log("new order requested");
+
+// });
+
+const userMenu = document.querySelector(".discord-user-menu");
+const userTab = document.getElementById("userTab");
+
+userTab.addEventListener("click", () => {
+    userMenu.classList.toggle("open");
+});
